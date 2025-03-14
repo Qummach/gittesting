44c7fd8ae6feb7a493b8e9f8bc22b725e0cec933
Qummach <andrey-zaharov-02@mail.ru>
2025-03-14 13:13:07 +0500

    nothing change

--- src/main/java/org/Java/Main.java
+++ src/main/java/org/Java/Main.java
@@ -2,6 +2,6 @@ package org.Java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Worlds!");
+        System.out.println("still main!");
     }
 }
