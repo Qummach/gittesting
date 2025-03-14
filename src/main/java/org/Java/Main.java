7904d1a5c8fdf473e84a0a6f31c183efd855da51
Qummach <andrey-zaharov-02@mail.ru>
2025-03-14 13:14:03 +0500

    develiope branch

--- src/main/java/org/Java/Main.java
+++ src/main/java/org/Java/Main.java
@@ -2,6 +2,6 @@ package org.Java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("still main!");
+        System.out.println("now develope branch!");
     }
 }
